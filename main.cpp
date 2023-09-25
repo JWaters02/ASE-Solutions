be2590840cae75e7336804f7277ef1b384541503
JWaters02 <watersjoshua2002@gmail.com>
2023-09-25 22:15:58 +0100

    week1 cmake

--- main.cpp
+++ main.cpp
@@ -1,22 +1,6 @@
 #include <iostream>
 
-#include "Week1/Coordinate.h"
-
 int main() {
-    Coordinate p1 = Coordinate(1,2); // stack allocation
-    Coordinate p2(3,7); // stack allocation
-    p1.xco = 4;
-    p2.yco = p2.yco + 5;
-    p1.display();
-    p2.display();
-
-    Coordinate* coPtr = new Coordinate(7,8);  // heap allocation
-    coPtr->display(); // same as (*coPtr).display();
-
-    Coordinate co2 = *coPtr;
-    Coordinate* coPtr2 = &co2;
-
-    delete coPtr; // heap deallocation
-    coPtr = nullptr; // set to null pointer to avoid dangling pointer
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
