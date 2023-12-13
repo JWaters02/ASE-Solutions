754273eb483c253ade1afb394b67f50f5738402e
JWaters02 <watersjoshua2002@gmail.com>
2023-12-13 12:47:25 +0000

    tidy up domino utils step 1

--- Dominoes/DominoUtils.h
+++ Dominoes/DominoUtils.h
@@ -10,6 +10,15 @@
 #include "DominoesConvoluted.h"
 
 class DominoUtils {
+private:
+    static std::string readFirstLine(const std::string& filename);
+
+    template<typename ReturnType>
+    static ReturnType createDomino(const std::string& line);
+
+    template<typename ReturnType>
+    static std::list<ReturnType> getInputDominoesGeneric(const std::string& filename);
+
 public:
     static DominoNode* getStartingDomino(const std::string& filename);
     static std::pair<std::string, std::string> getStartingDominoConvoluted(const std::string& filename);
@@ -17,97 +26,78 @@ public:
     static std::list<std::pair<std::string, std::string>> getInputDominoesConvoluted(const std::string& filename);
     static std::string getOutputDominoes(const std::string& filename);
 
-    template<class T>
-    static void createDominoLine(T& dominoLine);
+    template<class DominoImplementation>
+    static void createDominoLine(DominoImplementation& dominoLine);
 };
 
 DominoNode* DominoUtils::getStartingDomino(const std::string& filename) {
-    std::string line;
-
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        throw std::runtime_error("File not found");
-    }
-    std::getline(file, line);
-
-    if (const size_t colonPos = line.find(':'); colonPos != std::string::npos) {
-        const std::string leftSymbol = line.substr(0, colonPos);
-        const std::string rightSymbol = line.substr(colonPos + 1);
-        return new DominoNode(leftSymbol, rightSymbol);
-    }
-
-    throw std::runtime_error("Invalid starting domino");
+    std::string line = readFirstLine(filename);
+    return createDomino<DominoNode*>(line);
 }
 
-std::pair<std::string, std::string> DominoUtils::getStartingDominoConvoluted(const std::string &filename) {
-    std::string line;
-
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        throw std::runtime_error("File not found");
-    }
-    std::getline(file, line);
-
-    if (const size_t colonPos = line.find(':'); colonPos != std::string::npos) {
-        const std::string leftSymbol = line.substr(0, colonPos);
-        const std::string rightSymbol = line.substr(colonPos + 1);
-        return std::pair<std::string, std::string>(leftSymbol, rightSymbol);
-    }
-
-    throw std::runtime_error("Invalid starting domino");
+std::pair<std::string, std::string> DominoUtils::getStartingDominoConvoluted(const std::string& filename) {
+    std::string line = readFirstLine(filename);
+    return createDomino<std::pair<std::string, std::string>>(line);
 }
 
 std::list<DominoNode*> DominoUtils::getInputDominoes(const std::string& filename) {
-    std::list<DominoNode*> dominoes;
-    std::ifstream file(filename);
-    std::string line;
-
-    while (getline(file, line)) {
-        std::istringstream iss(line);
-        std::string leftSymbol, rightSymbol;
-        if (getline(iss, leftSymbol, ':') && getline(iss, rightSymbol)) {
-            dominoes.push_back(new DominoNode(leftSymbol, rightSymbol));
-        }
-    }
-
-    file.close();
-    return dominoes;
+    return getInputDominoesGeneric<DominoNode*>(filename);
 }
 
-std::list<std::pair<std::string, std::string>> DominoUtils::getInputDominoesConvoluted(const std::string &filename) {
-    std::list<std::pair<std::string, std::string>> dominoes;
-    std::ifstream file(filename);
-    std::string line;
-
-    while (getline(file, line)) {
-        std::istringstream iss(line);
-        std::string leftSymbol, rightSymbol;
-        if (getline(iss, leftSymbol, ':') && getline(iss, rightSymbol)) {
-            dominoes.push_back(std::pair<std::string, std::string>(leftSymbol, rightSymbol));
-        }
-    }
-
-    file.close();
-    return dominoes;
+std::list<std::pair<std::string, std::string>> DominoUtils::getInputDominoesConvoluted(const std::string& filename) {
+    return getInputDominoesGeneric<std::pair<std::string, std::string>>(filename);
 }
 
-std::string DominoUtils::getOutputDominoes(const std::string &filename) {
-    std::string outputDominoes = "";
-    std::ifstream file(filename);
-    std::string line;
-
-    std::getline(file, line);
-    outputDominoes += line;
-
-    file.close();
-    return outputDominoes;
+std::string DominoUtils::getOutputDominoes(const std::string& filename) {
+    return readFirstLine(filename);
 }
 
-template<class T>
-void DominoUtils::createDominoLine(T& dominoLine) {
+template<class DominoImplementation>
+void DominoUtils::createDominoLine(DominoImplementation& dominoLine) {
     while (!dominoLine.checkLineCompleted()) {
         dominoLine.addLeftDomino();
         if (dominoLine.checkLineCompleted()) break;
         dominoLine.addRightDomino();
+    }
+}
+
+std::string DominoUtils::readFirstLine(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        throw std::runtime_error("File not found");
+    }
+    std::string line;
+    std::getline(file, line);
+    return line;
+}
+
+template<typename ReturnType>
+std::list<ReturnType> DominoUtils::getInputDominoesGeneric(const std::string& filename) {
+    std::list<ReturnType> dominoes;
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        throw std::runtime_error("File not found");
+    }
+    std::string line;
+    while (getline(file, line)) {
+        dominoes.push_back(createDomino<ReturnType>(line));
+    }
+    return dominoes;
+}
+
+template<typename ReturnType>
+ReturnType DominoUtils::createDomino(const std::string& line) {
+    const size_t colonPos = line.find(':');
+    if (colonPos == std::string::npos) {
+        throw std::runtime_error("Invalid domino format");
+    }
+
+    const std::string leftSymbol = line.substr(0, colonPos);
+    const std::string rightSymbol = line.substr(colonPos + 1);
+
+    if constexpr (std::is_same_v<ReturnType, DominoNode*>) {
+        return new DominoNode(leftSymbol, rightSymbol);
+    } else {
+        return std::make_pair(leftSymbol, rightSymbol);
     }
 }
